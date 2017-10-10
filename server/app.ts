9c234a5cffd5d33a70c9fbe15508474b6f013588
Ehsan Asdar <ehsanmasdar@gmail.com>
2017-10-10 10:19:33 -0400

    Add live data

--- server/app.ts
+++ server/app.ts
@@ -2,6 +2,7 @@ import * as http from "http";
 import * as path from "path";
 import * as fs from "fs";
 import * as WebSocket from "ws";
+import * as Influx from "influx";
 
 import * as express from "express";
 import * as serveStatic from "serve-static";
@@ -26,16 +27,68 @@ app.use("/js", serveStatic(path.resolve(STATIC_ROOT, "js")));
 app.use("/css", serveStatic(path.resolve(STATIC_ROOT, "css")));
 app.use("/assets", serveStatic(path.resolve(STATIC_ROOT, "assets")));
 
+const influx = new Influx.InfluxDB({
+	host: process.env.INFLUX_URL || 'localhost',
+	database: 'metrics',
+	schema: [
+		{
+			measurement: 'https://checkin.hack.gt',
+			fields: {
+				value: Influx.FieldType.INTEGER
+			},
+			tags: [
+				'check_in',
+				'checked_in_by',
+				'checkinTag',
+				'email',
+				'id',
+				'name'
+			]
+		}
+	]
+});
 
 const server = http.createServer(app);
 export const wss = new WebSocket.Server({ server });
+const usersAlreadySeen: string[] = [];
+
 wss.on("connection", connection => {
 	connection.send(JSON.stringify({
 		"type": "count",
 		"count": 0
 	}));
+	connection.send(JSON.stringify({
+		"type": "users",
+		"count": usersAlreadySeen
+	}));
 });
 
+function getInfluxData() {
+	influx.query('SELECT count("value") FROM "https://checkin.hack.gt" GROUP BY "name";').then((data: any) => {
+		let count = data.length;
+		let newUsers = [];
+		for (let user of data) {
+			if (usersAlreadySeen.indexOf(user.name) === -1) {
+				usersAlreadySeen.push(user.name);
+				newUsers.push(user.name);
+			}
+		}
+		console.log(newUsers);
+		for (let socket of wss.clients) {
+			socket.send(JSON.stringify({
+				"type": "count",
+				"count": count
+			}));
+			socket.send(JSON.stringify({
+				"type": "users",
+				"users": newUsers
+			}));
+		}
+	}).catch((err) => {
+		console.error(err);
+	});
+}
+setInterval(getInfluxData, 5000);
 server.listen(PORT, () => {
 	console.log(`Registration system started on port ${PORT}`);
 });
