79795704573f3ad2a0e09464eb0009da9ce6ea5b
Ryan Petschek <petschekr@gmail.com>
2017-10-10 01:56:03 -0400

    Move console text into position

--- client/js/splash.ts
+++ client/js/splash.ts
@@ -10,6 +10,7 @@ let writeTextRunning: boolean = false;
 async function writeText(container: HTMLElement, text: string[], lineWaitTime: number = 1000) {
 	if (writeTextRunning) return;
 	writeTextRunning = true;
+	container.classList.remove("hide-cursor");
 	await wait(500);
 	container.classList.remove("idle");
 	for (let line of text) {
@@ -17,7 +18,7 @@ async function writeText(container: HTMLElement, text: string[], lineWaitTime: n
 		for (let char of line) {
 			container.textContent += char;
 			let waitTime = Math.random() * 20 + 100;
-			if ([",", ".", "!", "?", "\n"].indexOf(char) !== -1) {
+			if (["/", ",", ".", "!", "?", "\n"].indexOf(char) !== -1) {
 				waitTime *= 3;
 			}
 			await wait(waitTime);
@@ -31,17 +32,14 @@ async function writeText(container: HTMLElement, text: string[], lineWaitTime: n
 	writeTextRunning = false;
 }
 
-function generateCountdownText(): string {
-	const date = moment("2017-10-13");
-	let days = date.diff(moment(), "days");
-	if (days < 0) {
-		return "Thanks for coming!";
-	}
-	return `${days} day${days === 1 ? "" : "s"} remaining`;
-}
-
 window.onload = async () => {
-	await writeText(document.getElementById("system-active")!, [
-		"// HackGT System Active"
+	const systemActive = document.getElementById("system-active")!;
+	const status = document.getElementById("status")!;
+
+	await writeText(systemActive, [
+		"// HackGT system active"
+	]);
+	await writeText(status, [
+		"Stuff!"
 	]);
 };
